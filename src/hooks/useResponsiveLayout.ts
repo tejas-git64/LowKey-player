283e8930649b9b1d3e508974b24039b78971ef85
tejas-git64 <tejas0586@gmail.com>
2025-06-11 01:37:25 +0530

    added responsive hook

---
+++ src/hooks/useResponsiveLayout.ts
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+export const useResponsiveLayout = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 640);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isMobile;
+};
