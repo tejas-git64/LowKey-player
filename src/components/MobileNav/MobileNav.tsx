2e0fbb00cc95938f6d43c8b375c5fa5992a7d2de
tejas-git64 <tejas0586@gmail.com>
2025-06-11 01:38:33 +0530

    mobile nav

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
-import home from "../../assets/icons8-home.svg";
-import search from "../../assets/icons8-search.svg";
-import fav from "../../assets/icons8-heart.svg";
-import library from "../../assets/icons8-library.svg";
+import home from "../../assets/svgs/icons8-home.svg";
+import search from "../../assets/svgs/icons8-search.svg";
+import fav from "../../assets/svgs/icons8-heart.svg";
+import library from "../../assets/svgs/icons8-library.svg";
 
 export default function MobileNav() {
   return (
@@ -11,28 +11,28 @@ export default function MobileNav() {
         to={"/home"}
         className="flex flex-col items-center invert-[0.3] hover:invert-0"
       >
-        <img src={home} alt="home" className="h-[24px] w-[24px]" />
+        <img src={home} alt="home" className="h-[26px] w-[26px]" />
         <p className="text-xs text-neutral-200">Home</p>
       </Link>
       <Link
         to={"/search"}
         className="flex flex-col items-center invert-[0.3] hover:invert-0"
       >
-        <img src={search} alt="search" className="h-[24px] w-[24px]" />
+        <img src={search} alt="search" className="h-[25px] w-[25px]" />
         <p className="text-xs text-neutral-200">Search</p>
       </Link>
       <Link
         to={"/favorites"}
         className="flex flex-col items-center invert-[0.3] hover:invert-0"
       >
-        <img src={fav} alt="favorites" className="h-[24px] w-[24px]" />
+        <img src={fav} alt="favorites" className="h-[25.5px] w-[25.5px]" />
         <p className="text-xs text-neutral-200">Favorites</p>
       </Link>
       <Link
         to={"/library"}
         className="flex flex-col items-center invert-[0.3] hover:invert-0"
       >
-        <img src={library} alt="library" className="h-[24px] w-[24px]" />
+        <img src={library} alt="library" className="h-[23px] w-[23px]" />
         <p className="text-xs text-neutral-200">Library</p>
       </Link>
     </nav>
