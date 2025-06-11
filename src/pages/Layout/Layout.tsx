3eb39dd16e7879cf728e2d39c5a7dc98bc9ef7b2
tejas-git64 <tejas0586@gmail.com>
2025-06-11 20:46:54 +0530

    improved layout

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -4,17 +4,14 @@ import Menu from "../../components/Menu/Menu";
 import Recents from "../../components/Recents/Recents";
 import PlayingPill from "../../components/PlayingPill/PlayingPill";
 import MobileNav from "../../components/MobileNav/MobileNav";
-import { useBoundStore } from "../../store/store";
 import NowPlaying from "../../components/Nowplaying/NowPlaying";
-import MobilePlayer from "../../components/MobilePlayer/MobilePlayer";
 import Creation from "../../components/Creation/Creation";
 
 export default function Layout() {
   const path = useLocation().pathname;
-  const nowPlaying = useBoundStore((state) => state.nowPlaying);
 
   return (
-    <div className="h-full w-full overflow-hidden bg-black">
+    <div className="relative h-full w-full overflow-hidden 2xl:mx-auto 2xl:max-h-[1200px] 2xl:max-w-[1600px]">
       {path !== "/" && <Nav />}
       <div
         className={`relative flex h-full w-full flex-row items-start justify-start overflow-hidden ${
@@ -27,7 +24,7 @@ export default function Layout() {
             path !== "/" ? "sm:h-[95vh]" : "sm:h-full"
           }`}
         >
-          <div className="h-full w-full bg-black">
+          <div className="h-full w-full border-x-2 border-black bg-black">
             <Outlet />
           </div>
           <Creation />
@@ -36,14 +33,13 @@ export default function Layout() {
               path === "/" ? "hidden" : "sm:hidden"
             } flex h-auto w-screen flex-col items-center justify-end bg-gradient-to-t from-black via-[#000000dd] to-transparent pt-2`}
           >
-            {nowPlaying.track?.id !== "" && <PlayingPill />}
-            {nowPlaying.track?.id !== "" && path !== "/" && <MobilePlayer />}
+            <PlayingPill />
             <MobileNav />
           </div>
         </div>
         {path !== "/" && <Recents />}
-        {path !== "/" && <NowPlaying />}
       </div>
+      {path !== "/" && <NowPlaying />}
     </div>
   );
 }
