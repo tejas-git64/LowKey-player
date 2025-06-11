0f6e5fff6df101b9001b7d56cd96cc10c53a5c0e
tejas-git64 <tejas0586@gmail.com>
2025-06-11 13:09:33 +0530

    updated configs with new breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xmd: "850px",
+        xlg: "1150px",
+        xxl: "1450px",
+      },
+    },
   },
   plugins: [],
 };
