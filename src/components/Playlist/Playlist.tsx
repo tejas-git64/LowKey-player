52dcb082917142218f571e203efa39be234ece06
tejas-git64 <tejas0586@gmail.com>
2025-06-11 13:05:44 +0530

    added cosmetic updates

--- src/components/Playlist/Playlist.tsx
+++ src/components/Playlist/Playlist.tsx
@@ -8,16 +8,16 @@ export default function Playlist({ id, image, name }: PlaylistOfList) {
     <>
       <li
         key={id}
-        className="mr-4 flex h-[180px] w-[150px] flex-shrink-0 cursor-pointer list-none flex-col items-center"
+        className="group mr-4 flex h-[180px] w-[150px] flex-shrink-0 cursor-pointer list-none flex-col items-center"
         onClick={() => navigate(`/playlists/${id}`, { replace: true })}
       >
         <img
           src={image ? image[1].url : fallback}
           alt="user-profile"
-          className="h-[150px] w-[150px] shadow-md shadow-black"
+          className="h-[150px] w-[150px] rounded-none shadow-md shadow-black transition-all ease-linear group-hover:rounded-xl group-hover:brightness-110"
           onError={(e) => (e.currentTarget.src = fallback)}
         />
-        <p className="mt-2 line-clamp-1 text-ellipsis whitespace-pre-line text-center text-xs font-semibold text-neutral-400">
+        <p className="mt-2 line-clamp-1 text-ellipsis whitespace-pre-line text-center text-xs text-neutral-400 transition-colors ease-linear group-hover:text-white sm:text-sm">
           {name}
         </p>
       </li>
