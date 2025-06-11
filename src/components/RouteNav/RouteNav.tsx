3e2481fc66cfc8aa65d84e1c8684a82df8f051e4
tejas-git64 <tejas0586@gmail.com>
2025-06-11 13:13:11 +0530

    added few cosmetic changes

--- src/components/RouteNav/RouteNav.tsx
+++ src/components/RouteNav/RouteNav.tsx
@@ -1,31 +1,23 @@
 import { useNavigate } from "react-router-dom";
-import back from "../../assets/icons8-back.svg";
-import next from "../../assets/icons8-next.svg";
+import back from "../../assets/svgs/icons8-back.svg";
+import next from "../../assets/svgs/icons8-next.svg";
 
 export default function RouteNav() {
   const navigate = useNavigate();
 
   return (
-    <div className="max-w-auto ml-2 flex w-[65px] flex-shrink-0 items-center justify-between sm:w-[70px]">
+    <div className="max-w-auto ml-2 flex w-[45px] flex-shrink-0 items-center justify-between sm:w-[60px]">
       <button
         onClick={() => navigate(-1)} // go to previous page
-        style={{
-          border: "none",
-          outline: "none",
-        }}
         className="bg-transparent p-0"
       >
-        <img src={back} alt="back" className="h-[30px] w-[30px] hover:invert" />
+        <img src={back} alt="back" className="h-6 w-6 hover:invert" />
       </button>
       <button
         onClick={() => navigate(1)} //go to next page
-        style={{
-          border: "none",
-          outline: "none",
-        }}
         className="bg-transparent p-0"
       >
-        <img src={next} alt="next" className="h-[30px] w-[30px] hover:invert" />
+        <img src={next} alt="next" className="h-6 w-6 hover:invert" />
       </button>
     </div>
   );
