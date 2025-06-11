f5f8cd31f0a9ab271ec12982b5edf9578f6123e3
tejas-git64 <tejas0586@gmail.com>
2025-06-11 20:47:50 +0530

    added query debounce

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,25 +1,28 @@
 import { useDeferredValue, useEffect, useState } from "react";
 import { getSearchResults } from "../../api/requests";
-import searchIcon from "../../assets/icons8-search.svg";
+import searchIcon from "../../assets/svgs/icons8-search.svg";
 
 export default function Searchbar() {
   const [query, setQuery] = useState("");
   const deferredQuery = useDeferredValue(query);
 
   useEffect(() => {
-    if (query !== "") {
-      getSearchResults(deferredQuery.replace(" ", "+"));
+    const trimmed = deferredQuery.trim();
+    if (trimmed !== "") {
+      const handler = setTimeout(() => {
+        getSearchResults(trimmed.replace(/ +/g, "+"));
+      }, 500);
+      return () => clearTimeout(handler);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [deferredQuery]);
 
   return (
     <>
-      <div className="mx-auto mr-3 flex h-10 w-full items-center justify-start overflow-hidden rounded-md bg-neutral-900 sm:ml-4 lg:ml-3 xl:ml-2 2xl:ml-0">
+      <div className="mr-3 flex h-10 w-full items-center justify-start overflow-hidden rounded-sm bg-neutral-900 2xl:ml-0">
         <img
           src={searchIcon}
           alt="search-icon"
-          className="w-[35px] bg-neutral-900 py-3 pl-3 invert-[0.2] sm:w-auto sm:p-2 sm:pr-1"
+          className="w-[35px] bg-neutral-900 py-3 pl-2 pr-1 invert-[0.2] sm:w-auto sm:p-2 sm:pr-1"
         />
         <input
           type="search"
@@ -28,11 +31,10 @@ export default function Searchbar() {
             wordSpacing: "3px",
           }}
           value={query}
-          onChange={(e) => {
-            setQuery(e.target.value);
-          }}
+          aria-label="searchbar"
+          onChange={(e) => setQuery(e.target.value)}
           placeholder="Search for songs, albums, playlists or artists"
-          className="placeholder: h-full w-full border-none bg-neutral-700 px-2 font-semibold text-neutral-300 outline-none placeholder:font-medium placeholder:text-neutral-400"
+          className="placeholder: h-full w-full border-none bg-neutral-700 px-2 font-medium text-neutral-300 outline-none placeholder:font-medium placeholder:text-neutral-400"
         />
       </div>
     </>
