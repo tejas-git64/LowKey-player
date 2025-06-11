679295c55ef07aa5d866a88dbfc06f2cf1ee3f52
tejas-git64 <tejas0586@gmail.com>
2025-06-12 00:33:51 +0530

    added reusable utility function

---
+++ src/helpers/handleCollectionPlayback.ts
@@ -0,0 +1,25 @@
+import { TrackDetails } from "../types/GlobalTypes";
+
+const handleCollectionPlayback = (
+  e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  collection: { id: string; name: string; image: any; songs: TrackDetails[] },
+  isPlaying: boolean,
+  setQueue: Function,
+  setNowPlaying: Function,
+  setIsPlaying: Function,
+) => {
+  e.stopPropagation();
+  if (!isPlaying) {
+    setQueue({
+      id: collection.id,
+      name: collection.name,
+      image: collection.image || false,
+      songs: collection.songs,
+    });
+    setNowPlaying(collection.songs[0]);
+    setIsPlaying(true);
+  } else {
+    setIsPlaying(false);
+  }
+};
+export default handleCollectionPlayback;
