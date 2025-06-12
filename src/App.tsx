a8943cce1889667dbe5752c3d513231559ed51df
tejas-git64 <tejas0586@gmail.com>
2025-06-12 22:57:23 +0530

    code splitting enabled

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,21 @@ import {
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
-import Layout from "./pages/Layout/Layout";
-import Intro from "./pages/Intro/Intro";
-import Home from "./pages/Home/Home";
-import Search from "./pages/Search/Search";
-import PlaylistPage from "./pages/Playlist/PlaylistPage";
-import Library from "./pages/Library/Library";
-import Favorites from "./pages/Favorites/Favorites";
-import ArtistPage from "./pages/Artist/ArtistPage";
-import UserPlaylistPage from "./pages/UserPlaylist/UserPlaylist";
-import NotFound from "./pages/NotFound/NotFound";
-import AlbumPage from "./pages/Album/AlbumPage";
+import { lazy } from "react";
+
+const Layout = lazy(() => import("./pages/Layout/Layout"));
+const Intro = lazy(() => import("./pages/Intro/Intro"));
+const Home = lazy(() => import("./pages/Home/Home"));
+const Search = lazy(() => import("./pages/Search/Search"));
+const PlaylistPage = lazy(() => import("./pages/Playlist/PlaylistPage"));
+const Library = lazy(() => import("./pages/Library/Library"));
+const Favorites = lazy(() => import("./pages/Favorites/Favorites"));
+const ArtistPage = lazy(() => import("./pages/Artist/ArtistPage"));
+const UserPlaylistPage = lazy(
+  () => import("./pages/UserPlaylist/UserPlaylist"),
+);
+const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
+const AlbumPage = lazy(() => import("./pages/Album/AlbumPage"));
 
 export default function App() {
   const queryClient = new QueryClient();
