512a57f06ebb8e534b0f82c3e929b1c72adc4d4a
tejas-git64 <tejas0586@gmail.com>
2025-06-25 17:03:49 +0530

    added smooth animations and transitions

--- src/components/Recents/Recents.tsx
+++ src/components/Recents/Recents.tsx
@@ -4,7 +4,7 @@ import { useBoundStore } from "../../store/store";
 import { ActivityType, TrackDetails } from "../../types/GlobalTypes";
 import songfallback from "../../assets/fallbacks/song-fallback.webp";
 import { cleanString } from "../../helpers/cleanString";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 type RecentTypes = {
   history: TrackDetails[] | null;
@@ -38,8 +38,8 @@ export default function Recents() {
           <img src={history} alt="history" className="h-[20px] w-[20px]" />
         </div>
         <ul className="flex h-full w-full list-none flex-col items-center justify-start overflow-y-scroll bg-neutral-900">
-          {recentTracks?.map((song: TrackDetails) => (
-            <RecentSong key={song.id} {...song} />
+          {recentTracks?.map((song: TrackDetails, i) => (
+            <RecentSong key={song.id} i={i} {...song} />
           ))}
         </ul>
       </div>
@@ -49,8 +49,8 @@ export default function Recents() {
           <img src={activity} alt="activity" className="h-[25px] w-[25px]" />
         </div>
         <ul className="h-[38dvh] w-full list-none overflow-y-scroll bg-neutral-900">
-          {recentActivity.map((message: string) => (
-            <Activity key={message} message={message} />
+          {recentActivity.map((message: string, i) => (
+            <Activity key={i} i={i} message={message} />
           ))}
         </ul>
       </div>
@@ -58,12 +58,27 @@ export default function Recents() {
   );
 }
 
-const RecentSong = ({ id, name, artists, image }: TrackDetails) => {
+const RecentSong = ({
+  id,
+  name,
+  artists,
+  image,
+  i,
+}: TrackDetails & { i: number }) => {
+  const recentEl = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      recentEl.current?.classList.remove("song-fadeout");
+      recentEl.current?.classList.add("song-fadein");
+    }, i * 50);
+  }, []);
   return (
     <>
       <li
         id={id}
-        className="mb-0.5 flex h-[50px] w-full flex-shrink-0 items-center justify-start overflow-hidden bg-black"
+        ref={recentEl}
+        className="song-fadeout mb-0.5 flex h-[50px] w-full flex-shrink-0 items-center justify-start overflow-hidden bg-black duration-200 ease-in"
       >
         <img
           src={image ? image[0]?.url : songfallback}
@@ -85,10 +100,22 @@ const RecentSong = ({ id, name, artists, image }: TrackDetails) => {
   );
 };
 
-const Activity = ({ message }: ActivityType) => {
+const Activity = ({ message, i }: ActivityType & { i: number }) => {
+  const activityEl = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      activityEl.current?.classList.remove("song-fadeout");
+      activityEl.current?.classList.add("song-fadein");
+    }, i * 50);
+  }, []);
+
   return (
     <>
-      <li className="mb-0.5 line-clamp-1 flex h-[35px] w-full flex-shrink-0 items-center justify-start overflow-hidden text-ellipsis whitespace-nowrap bg-black px-2">
+      <li
+        ref={activityEl}
+        className="song-fadeout mb-0.5 line-clamp-1 flex h-[35px] w-full flex-shrink-0 items-center justify-start overflow-hidden text-ellipsis whitespace-nowrap bg-black px-2 duration-200 ease-in"
+      >
         <p className="mx-1 line-clamp-1 w-auto text-ellipsis text-xs font-semibold text-neutral-300">
           {message}
         </p>
