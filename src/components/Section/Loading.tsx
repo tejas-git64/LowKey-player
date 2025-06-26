be4f453ddc2fcf0b7c8813c9a1cc8404ff8796ec
tejas-git64 <tejas0586@gmail.com>
2025-06-26 18:53:19 +0530

    modified fallback for sections

--- src/components/Section/Loading.tsx
+++ src/components/Section/Loading.tsx
@@ -4,30 +4,15 @@ export default function SectionLoading() {
       <div className="mb-2 flex h-auto w-full animate-pulse flex-col overflow-x-hidden bg-transparent py-2 pt-3">
         <div className="ml-4 h-[20px] w-44 rounded-full bg-neutral-700 px-4 py-1"></div>
         <ul className="flex h-48 w-full overflow-hidden whitespace-nowrap p-4">
-          <li className="mr-4 flex h-[180px] w-[150px] flex-shrink-0 list-none flex-col items-center">
-            <div className="line-clamp-1 h-[150px] w-full bg-neutral-700"></div>
-            <div className="mt-2 line-clamp-1 h-3 w-full rounded-full bg-neutral-700"></div>
-          </li>
-          <li className="mr-4 flex h-[180px] w-[150px] flex-shrink-0 list-none flex-col items-center">
-            <div className="line-clamp-1 h-[150px] w-full bg-neutral-700"></div>
-            <div className="mt-2 line-clamp-1 h-3 w-full rounded-full bg-neutral-700"></div>
-          </li>
-          <li className="mr-4 flex h-[180px] w-[150px] flex-shrink-0 list-none flex-col items-center">
-            <div className="line-clamp-1 h-[150px] w-full bg-neutral-700"></div>
-            <div className="mt-2 line-clamp-1 h-3 w-full rounded-full bg-neutral-700"></div>
-          </li>
-          <li className="mr-4 flex h-[180px] w-[150px] flex-shrink-0 list-none flex-col items-center">
-            <div className="line-clamp-1 h-[150px] w-full bg-neutral-700"></div>
-            <div className="mt-2 line-clamp-1 h-3 w-full rounded-full bg-neutral-700"></div>
-          </li>
-          <li className="mr-4 flex h-[180px] w-[150px] flex-shrink-0 list-none flex-col items-center">
-            <div className="line-clamp-1 h-[150px] w-full bg-neutral-700"></div>
-            <div className="mt-2 line-clamp-1 h-3 w-full rounded-full bg-neutral-700"></div>
-          </li>
-          <li className="mr-4 flex h-[180px] w-[150px] flex-shrink-0 list-none flex-col items-center">
-            <div className="line-clamp-1 h-[150px] w-full bg-neutral-700"></div>
-            <div className="mt-2 line-clamp-1 h-3 w-full rounded-full bg-neutral-700"></div>
-          </li>
+          {Array.from({ length: 5 }).map((_, i) => (
+            <li
+              key={i}
+              className="mr-4 flex h-[180px] w-[150px] flex-shrink-0 list-none flex-col items-center"
+            >
+              <div className="line-clamp-1 h-[150px] w-full bg-neutral-700"></div>
+              <div className="mt-2 line-clamp-1 h-3 w-full rounded-full bg-neutral-700"></div>
+            </li>
+          ))}
         </ul>
       </div>
     </>
