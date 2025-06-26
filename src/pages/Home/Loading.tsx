95cc3985a4e0f148527f09bfb16e8aec9cbf73ea
tejas-git64 <tejas0586@gmail.com>
2025-06-26 18:53:36 +0530

    improved fallbacks for home components

--- src/pages/Home/Loading.tsx
+++ src/pages/Home/Loading.tsx
@@ -1,60 +1,58 @@
 import SongFallback from "../../components/Song/SongFallback";
 
-export default function Loading() {
+export const Widgetfallback = () => {
+  return (
+    <div className="h-auto max-h-max w-full px-3.5 sm:pt-0.5">
+      <section className="relative z-0 mx-auto mb-7 flex h-80 w-full flex-col overflow-hidden rounded-sm bg-neutral-900 sm:my-3 sm:h-[40vw] sm:flex-row sm:bg-transparent md:h-80">
+        <div className="h-auto w-auto flex-shrink-0 cursor-pointer rounded-sm bg-neutral-700 duration-500 ease-in sm:z-10 sm:h-[40vw] sm:w-[40vw] md:h-80 md:w-80"></div>
+        <div className="absolute right-2.5 top-[105px] z-20 flex h-auto w-[95%] items-end justify-between sm:-left-16 sm:top-[80%] sm:h-12 sm:w-[48vw] sm:justify-end sm:p-2 md:w-[370px] md:py-1">
+          <div className="left-0 top-0 h-9 w-[80%] rounded-sm bg-neutral-500 pl-1 text-xl font-bold sm:hidden"></div>
+          <div className="-mb-1 animate-pulse rounded-full bg-neutral-400 p-[22px] sm:mb-0"></div>
+        </div>
+        <ul
+          id="widget-container"
+          className="absolute bottom-1.5 left-[1.75%] h-[158px] w-[96.5%] list-none overflow-x-hidden overflow-y-scroll rounded-sm bg-neutral-900 sm:static sm:ml-3 sm:mt-0 sm:h-full"
+        >
+          {Array.from({ length: 8 }).map((_, i) => (
+            <SongFallback key={i} isWidgetSong={false} />
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export const TimelyFallback = () => {
+  return (
+    <div className="mx-auto mb-5 mt-2 grid h-32 w-full grid-cols-2 grid-rows-2 gap-3 px-3.5 sm:gap-5">
+      <div className="flex h-12 w-full items-center justify-start overflow-hidden rounded-sm bg-neutral-800 shadow-sm sm:h-14">
+        <div className="h-full w-12 animate-pulse bg-neutral-500 sm:w-14"></div>
+        <div className="mx-4 h-4 w-[50%] animate-pulse bg-neutral-600 sm:h-6"></div>
+      </div>
+      <div className="flex h-12 w-full items-center justify-start overflow-hidden rounded-sm bg-neutral-800 shadow-sm sm:h-14">
+        <div className="h-full w-12 animate-pulse bg-neutral-500 sm:w-14"></div>
+        <div className="mx-4 h-4 w-[50%] animate-pulse bg-neutral-600 sm:h-6"></div>
+      </div>
+      <div className="flex h-12 w-full items-center justify-start overflow-hidden rounded-sm bg-neutral-800 shadow-sm sm:h-14">
+        <div className="h-full w-12 animate-pulse bg-neutral-500 sm:w-14"></div>
+        <div className="mx-4 h-4 w-[50%] animate-pulse bg-neutral-600 sm:h-6"></div>
+      </div>
+      <div className="flex h-12 w-full items-center justify-start overflow-hidden rounded-sm bg-neutral-800 shadow-sm sm:h-14">
+        <div className="h-full w-12 animate-pulse bg-neutral-500 sm:w-14"></div>
+        <div className="mx-4 h-4 w-[50%] animate-pulse bg-neutral-600 sm:h-6"></div>
+      </div>
+    </div>
+  );
+};
+
+export const Loading = () => {
   return (
     <>
       <div className="h-auto w-full bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-black via-neutral-950 to-neutral-700">
         <div className="h-auto w-full pt-4 sm:pt-2 xl:w-full">
           <div className="mx-4 -mb-0.5 h-5 w-44 animate-pulse rounded-md bg-neutral-700 sm:hidden"></div>
-          <div className="h-auto w-full px-3.5">
-            <section className="relative mx-auto mb-7 mt-4 flex h-80 w-full animate-pulse flex-col overflow-hidden rounded-2xl bg-transparent sm:mt-3 sm:flex-row">
-              <div className="h-full w-auto flex-shrink-0 bg-neutral-700 sm:z-10 sm:h-full sm:w-[320px] sm:rounded-xl"></div>
-              <div className="absolute right-2.5 top-[105px] z-20 flex h-10 w-[95%] items-end justify-between sm:left-0 sm:top-[268px] sm:h-12 sm:w-[320px] sm:p-2 md:justify-end md:p-2 md:py-1">
-                <div className="left-0 top-0 line-clamp-1 h-5 w-[80%] rounded-full bg-neutral-500 pl-1 text-xl font-bold sm:hidden"></div>
-                <div className="-mb-1 rounded-full bg-neutral-300 p-[22px] sm:mb-0"></div>
-              </div>
-              <ul
-                id="widget-container"
-                className="absolute bottom-1.5 left-1.5 mx-auto h-[158px] w-[96.5%] list-none overflow-hidden rounded-xl bg-neutral-900 sm:static sm:ml-3 sm:mt-0 sm:h-full sm:w-[80%]"
-              >
-                <SongFallback />
-                <SongFallback />
-                <SongFallback />
-                <SongFallback />
-                <SongFallback />
-                <SongFallback />
-                <SongFallback />
-                <SongFallback />
-              </ul>
-            </section>
-          </div>
           <div className="mx-4 -mb-1 h-6 w-44 animate-pulse rounded-lg bg-neutral-700"></div>
-          <div className="mx-auto my-4 mt-6 grid h-auto w-full grid-cols-2 grid-rows-2 gap-3 px-3.5 sm:gap-5">
-            <div className="flex h-12 w-full animate-pulse overflow-hidden rounded-md bg-neutral-800 sm:h-full">
-              <div className="h-full w-12 bg-neutral-500 sm:w-14"></div>
-              <div className="sm:text-md p-4">
-                <div className="mt-0.5 h-3 w-28 rounded-lg bg-neutral-600 sm:mt-0 sm:h-6"></div>
-              </div>
-            </div>
-            <div className="flex h-12 w-full animate-pulse overflow-hidden rounded-md bg-neutral-800 sm:h-full">
-              <div className="h-full w-12 bg-neutral-500 sm:w-14"></div>
-              <div className="sm:text-md p-4">
-                <div className="mt-0.5 h-3 w-28 rounded-lg bg-neutral-600 sm:mt-0 sm:h-6"></div>
-              </div>
-            </div>
-            <div className="flex h-12 w-full animate-pulse overflow-hidden rounded-md bg-neutral-800 sm:h-full">
-              <div className="h-full w-12 bg-neutral-500 sm:w-14"></div>
-              <div className="sm:text-md p-4">
-                <div className="mt-0.5 h-3 w-28 rounded-lg bg-neutral-600 sm:mt-0 sm:h-6"></div>
-              </div>
-            </div>
-            <div className="flex h-12 w-full animate-pulse overflow-hidden rounded-md bg-neutral-800 sm:h-full">
-              <div className="h-full w-12 bg-neutral-500 sm:w-14"></div>
-              <div className="sm:text-md p-4">
-                <div className="mt-0.5 h-3 w-28 rounded-lg bg-neutral-600 sm:mt-0 sm:h-6"></div>
-              </div>
-            </div>
-          </div>
+
           {/* Trending */}
           <div className="flex h-auto w-full animate-pulse flex-col bg-transparent py-3">
             <div className="ml-4 h-[20px] w-28 rounded-md bg-neutral-500 px-4 pb-1"></div>
@@ -135,4 +133,4 @@ export default function Loading() {
       </div>
     </>
   );
-}
+};
