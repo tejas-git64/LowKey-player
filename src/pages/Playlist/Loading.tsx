87a0e69fca87c48a5fc721179ae237c595dc25ee
tejas-git64 <tejas0586@gmail.com>
2025-06-26 18:54:43 +0530

    improved fallbacks for lists

--- src/pages/Playlist/Loading.tsx
+++ src/pages/Playlist/Loading.tsx
@@ -5,36 +5,26 @@ export default function ListLoading() {
     <div className="h-full w-full overflow-x-hidden overflow-y-scroll scroll-smooth">
       <div className="flex h-auto w-full flex-col items-center justify-center border-b border-black bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-black via-neutral-950 to-neutral-700 p-3 sm:h-[23.5dvh] sm:px-4">
         <div className="flex h-auto w-full flex-col items-start justify-start pt-1 sm:flex-row sm:items-center sm:pt-1.5">
-          <div className="mr-4 h-[150px] w-[150px] bg-neutral-500"></div>
-          <div className="mb-1 mt-3.5 h-[16px] w-[50%] rounded-full bg-neutral-400 sm:line-clamp-3 sm:h-[30px] sm:w-[40%] sm:leading-9"></div>
+          <div className="mr-4 h-[150px] w-[150px] animate-pulse bg-neutral-500"></div>
+          <div className="text-md mt-2 h-4 w-[60%] rounded-sm bg-neutral-500 sm:h-8 sm:w-[40%] sm:text-3xl"></div>
         </div>
         <div className="flex h-auto w-full items-center justify-between">
           <div className="flex h-auto w-[50%] flex-col items-start justify-center sm:mt-1 sm:w-[30%]">
-            <div className="my-2 mb-0 h-[13px] w-[90%] rounded-full bg-neutral-500 leading-tight sm:line-clamp-2"></div>
-            <div className="mb-1 mt-3 h-[13px] w-32 rounded-full bg-neutral-500"></div>
+            <div className="my-2 h-[14px] w-[90%] rounded-sm bg-neutral-500 leading-tight sm:mt-1 sm:line-clamp-2"></div>
+            <div className="mr-2 h-[14px] w-32 rounded-sm bg-neutral-500"></div>
           </div>
-          <div className="flex h-auto w-[180px] items-center justify-between">
-            <div className="rounded-ful h-[28px] w-[28px] rounded-full bg-neutral-500 p-0"></div>
-            <div className="h-[28px] w-[28px] rounded-full bg-neutral-500 p-0"></div>
-            <div className="h-[28px] w-[28px] rounded-md bg-neutral-500 p-0"></div>
-            <div className="h-[41px] w-[41px] rounded-full bg-neutral-400"></div>
+          <div className="mr-1 flex w-[170px] items-center justify-between sm:mr-0">
+            <div className="-mr-1 h-6 w-6 rounded-sm bg-neutral-500 p-0"></div>
+            <div className="h-6 w-6 rounded-sm bg-neutral-500"></div>
+            <div className="h-6 w-6 rounded-sm bg-neutral-500"></div>
+            <div className="h-9 w-9 rounded-full bg-neutral-400"></div>
           </div>
         </div>
       </div>
-      <ul className="flex h-auto w-full flex-col items-start justify-start bg-neutral-900 px-3 py-2">
-        <SongFallback />
-        <SongFallback />
-        <SongFallback />
-        <SongFallback />
-        <SongFallback />
-        <SongFallback />
-        <SongFallback />
-        <SongFallback />
-        <SongFallback />
-        <SongFallback />
-        <SongFallback />
-        <SongFallback />
-        <SongFallback />
+      <ul className="flex h-auto min-h-[71.5dvh] w-full flex-col items-start justify-start bg-neutral-900 p-3 pb-28 sm:p-4 sm:pb-20">
+        {Array.from({ length: 15 }).map((_, i) => (
+          <SongFallback key={i} isWidgetSong={false} />
+        ))}
       </ul>
     </div>
   );
