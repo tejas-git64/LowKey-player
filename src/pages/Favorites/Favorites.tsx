bba006dab9961e6ac227e16607bd7f6036b5c346
tejas-git64 <tejas0586@gmail.com>
2025-10-02 22:23:07 +0530

    added test ids and improved guard checks

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -8,7 +8,7 @@ import favoritesImg from "../../assets/images/favorites.webp";
 import { useNavigate } from "react-router-dom";
 import fallback from "../../assets/fallbacks/playlist-fallback.webp";
 import close from "../../assets/svgs/close.svg";
-import { memo, useEffect, useMemo, useRef } from "react";
+import { memo, startTransition, useEffect, useMemo, useRef } from "react";
 import handleCollectionPlayback from "../../helpers/handleCollectionPlayback";
 import { saveToLocalStorage } from "../../helpers/saveToLocalStorage";
 import { animateScreen } from "../../helpers/animateScreen";
@@ -33,6 +33,7 @@ export default function Favorites() {
 
   return (
     <div
+      data-testid="favorites-page"
       ref={favEl}
       className="home-fadeout h-full w-full overflow-x-hidden overflow-y-scroll scroll-smooth pb-20 duration-200 ease-in"
     >
@@ -58,7 +59,10 @@ export default function Favorites() {
         <FavoriteControls />
       </div>
       {albums.length === 0 && playlists.length === 0 && songs.length === 0 ? (
-        <div className="flex min-h-[70dvh] w-full items-center justify-center bg-neutral-900">
+        <div
+          data-testid="no-favorites"
+          className="flex min-h-[70dvh] w-full items-center justify-center bg-neutral-900"
+        >
           <p className="font-normal text-neutral-500">No favorites added</p>
         </div>
       ) : (
@@ -119,7 +123,10 @@ const FavoriteControls = memo(() => {
   }, []);
 
   return (
-    <div className="mb-0 flex w-[120px] items-center justify-between sm:w-[105px]">
+    <div
+      data-testid="favorite-controls"
+      className="mb-0 flex w-[120px] items-center justify-between sm:w-[105px]"
+    >
       <button
         type="button"
         tabIndex={0}
@@ -146,6 +153,7 @@ const FavoriteControls = memo(() => {
             <path
               d="M16.4697 9.46967C16.1768 9.76256 16.1768 10.2374 16.4697 10.5303C16.7626 10.8232 17.2374 10.8232 17.5303 10.5303L16.4697 9.46967ZM19.5303 8.53033C19.8232 8.23744 19.8232 7.76256 19.5303 7.46967C19.2374 7.17678 18.7626 7.17678 18.4697 7.46967L19.5303 8.53033ZM18.4697 8.53033C18.7626 8.82322 19.2374 8.82322 19.5303 8.53033C19.8232 8.23744 19.8232 7.76256 19.5303 7.46967L18.4697 8.53033ZM17.5303 5.46967C17.2374 5.17678 16.7626 5.17678 16.4697 5.46967C16.1768 5.76256 16.1768 6.23744 16.4697 6.53033L17.5303 5.46967ZM19 8.75C19.4142 8.75 19.75 8.41421 19.75 8C19.75 7.58579 19.4142 7.25 19 7.25V8.75ZM16.7 8L16.6993 8.75H16.7V8ZM12.518 10.252L13.1446 10.6642L13.1446 10.6642L12.518 10.252ZM10.7414 11.5878C10.5138 11.9338 10.6097 12.3989 10.9558 12.6266C11.3018 12.8542 11.7669 12.7583 11.9946 12.4122L10.7414 11.5878ZM11.9946 12.4122C12.2222 12.0662 12.1263 11.6011 11.7802 11.3734C11.4342 11.1458 10.9691 11.2417 10.7414 11.5878L11.9946 12.4122ZM10.218 13.748L9.59144 13.3358L9.59143 13.3358L10.218 13.748ZM6.041 16V16.75H6.04102L6.041 16ZM5 15.25C4.58579 15.25 4.25 15.5858 4.25 16C4.25 16.4142 4.58579 16.75 5 16.75V15.25ZM11.9946 11.5878C11.7669 11.2417 11.3018 11.1458 10.9558 11.3734C10.6097 11.6011 10.5138 12.0662 10.7414 12.4122L11.9946 11.5878ZM12.518 13.748L13.1446 13.3358L13.1446 13.3358L12.518 13.748ZM16.7 16V15.25H16.6993L16.7 16ZM19 16.75C19.4142 16.75 19.75 16.4142 19.75 16C19.75 15.5858 19.4142 15.25 19 15.25V16.75ZM10.7414 12.4122C10.9691 12.7583 11.4342 12.8542 11.7802 12.6266C12.1263 12.3989 12.2222 11.9338 11.9946 11.5878L10.7414 12.4122ZM10.218 10.252L9.59143 10.6642L9.59144 10.6642L10.218 10.252ZM6.041 8L6.04102 7.25H6.041V8ZM5 7.25C4.58579 7.25 4.25 7.58579 4.25 8C4.25 8.41421 4.58579 8.75 5 8.75V7.25ZM17.5303 13.4697C17.2374 13.1768 16.7626 13.1768 16.4697 13.4697C16.1768 13.7626 16.1768 14.2374 16.4697 14.5303L17.5303 13.4697ZM18.4697 16.5303C18.7626 16.8232 19.2374 16.8232 19.5303 16.5303C19.8232 16.2374 19.8232 15.7626 19.5303 15.4697L18.4697 16.5303ZM19.5303 16.5303C19.8232 16.2374 19.8232 15.7626 19.5303 15.4697C19.2374 15.1768 18.7626 15.1768 18.4697 15.4697L19.5303 16.5303ZM16.4697 17.4697C16.1768 17.7626 16.1768 18.2374 16.4697 18.5303C16.7626 18.8232 17.2374 18.8232 17.5303 18.5303L16.4697 17.4697ZM17.5303 10.5303L19.5303 8.53033L18.4697 7.46967L16.4697 9.46967L17.5303 10.5303ZM19.5303 7.46967L17.5303 5.46967L16.4697 6.53033L18.4697 8.53033L19.5303 7.46967ZM19 7.25H16.7V8.75H19V7.25ZM16.7007 7.25C14.7638 7.24812 12.956 8.22159 11.8914 9.8398L13.1446 10.6642C13.9314 9.46813 15.2676 8.74861 16.6993 8.75L16.7007 7.25ZM11.8914 9.83979L10.7414 11.5878L11.9946 12.4122L13.1446 10.6642L11.8914 9.83979ZM10.7414 11.5878L9.59144 13.3358L10.8446 14.1602L11.9946 12.4122L10.7414 11.5878ZM9.59143 13.3358C8.80541 14.5306 7.47115 15.25 6.04098 15.25L6.04102 16.75C7.97596 16.7499 9.78113 15.7767 10.8446 14.1602L9.59143 13.3358ZM6.041 15.25H5V16.75H6.041V15.25ZM10.7414 12.4122L11.8914 14.1602L13.1446 13.3358L11.9946 11.5878L10.7414 12.4122ZM11.8914 14.1602C12.956 15.7784 14.7638 16.7519 16.7007 16.75L16.6993 15.25C15.2676 15.2514 13.9314 14.5319 13.1446 13.3358L11.8914 14.1602ZM16.7 16.75H19V15.25H16.7V16.75ZM11.9946 11.5878L10.8446 9.83979L9.59144 10.6642L10.7414 12.4122L11.9946 11.5878ZM10.8446 9.8398C9.78113 8.2233 7.97596 7.25005 6.04102 7.25L6.04098 8.75C7.47115 8.75004 8.80541 9.46939 9.59143 10.6642L10.8446 9.8398ZM6.041 7.25H5V8.75H6.041V7.25ZM16.4697 14.5303L18.4697 16.5303L19.5303 15.4697L17.5303 13.4697L16.4697 14.5303ZM18.4697 15.4697L16.4697 17.4697L17.5303 18.5303L19.5303 16.5303L18.4697 15.4697Z"
               fill="currentColor"
+              data-testid="svg-color"
               className={`${isFavoritePlaying ? (isShuffling ? "fill-emerald-500" : "fill-white") : "fill-neutral-500"} ease duration-250 transition-colors`}
             />
           </g>
@@ -160,6 +168,7 @@ const FavoriteControls = memo(() => {
           handleCollectionPlayback(
             e,
             favoriteCollection,
+            startTransition,
             isPlaying,
             inQueue,
             setQueue,
@@ -191,7 +200,7 @@ const FavoriteAlbums = memo(() => {
   }, [queue?.id]);
 
   return (
-    <>
+    <div data-testid="favorite-albums" className="h-auto w-full">
       {albums && albums.length > 0 && (
         <>
           <h2 className="p-4 py-2 font-semibold text-white">Albums</h2>
@@ -207,7 +216,7 @@ const FavoriteAlbums = memo(() => {
           </div>
         </>
       )}
-    </>
+    </div>
   );
 });
 
@@ -246,21 +255,17 @@ const FavoriteAlbum = ({
     <div
       tabIndex={0}
       key={album.id}
+      data-testid="favorite-album"
       className="group relative mr-4 flex h-[180px] w-[150px] flex-shrink-0 flex-col items-center bg-transparent outline-none"
       onClick={() => navigate(`/albums/${album.id}`, { replace: true })}
       role="link"
       aria-label={`Go to album ${album.name}`}
-      onKeyDown={(e) => {
-        if (e.key === "Enter" || e.key === " ") {
-          navigate(`/albums/${album.id}`, { replace: true });
-        }
-      }}
     >
       <div className="h-[150px] w-[150px] overflow-hidden">
         <img
           ref={albumImgEl}
           src={album.image[1]?.url || fallback}
-          alt="user-profile"
+          alt="album-image"
           width={150}
           height={150}
           className="image-fadeout scale-105 shadow-xl shadow-neutral-950 transition-transform duration-200 ease-in group-hover:scale-100 group-focus:scale-100"
@@ -274,6 +279,7 @@ const FavoriteAlbum = ({
         <button
           type="button"
           tabIndex={0}
+          data-testid="album-play-btn"
           className="rounded-full bg-emerald-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500"
           aria-label={
             isPlaying && album.id === queueId ? "Pause album" : "Play album"
@@ -283,6 +289,7 @@ const FavoriteAlbum = ({
             handleCollectionPlayback(
               e,
               album,
+              startTransition,
               isPlaying,
               inQueue,
               setQueue,
@@ -301,6 +308,7 @@ const FavoriteAlbum = ({
         <button
           type="button"
           tabIndex={0}
+          data-testid="album-remove-btn"
           className="ml-2 h-auto w-auto rounded-full bg-white p-0 focus:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500"
           aria-label={`Remove album ${album.name} from favorites`}
           onClick={(e) => {
@@ -319,7 +327,7 @@ const FavoritePlaylists = memo(() => {
   const playlists = useBoundStore((state) => state.favorites.playlists);
 
   return (
-    <>
+    <div data-testid="favorite-playlists" className="h-auto w-full">
       {playlists && playlists.length > 0 && (
         <>
           <h2 className="p-4 py-2 font-semibold text-white">Playlists</h2>
@@ -330,7 +338,7 @@ const FavoritePlaylists = memo(() => {
           </div>
         </>
       )}
-    </>
+    </div>
   );
 });
 
@@ -368,20 +376,16 @@ const FavoritePlaylist = ({
     <div
       key={playlist.id}
       tabIndex={0}
+      data-testid="favorite-playlist"
       className="group relative mr-4 flex h-[180px] w-[150px] flex-shrink-0 flex-col items-center bg-transparent outline-none"
-      onClick={() => navigate(`/albums/${playlist.id}`, { replace: true })}
+      onClick={() => navigate(`/playlists/${playlist.id}`, { replace: true })}
       role="link"
       aria-label={`Go to playlist ${playlist.name}`}
-      onKeyDown={(e) => {
-        if (e.key === "Enter" || e.key === " ") {
-          navigate(`/albums/${playlist.id}`, { replace: true });
-        }
-      }}
     >
       <div className="h-[150px] w-[150px] overflow-hidden">
         <img
           src={playlist.image[1]?.url || fallback}
-          alt="user-profile"
+          alt="playlist-image"
           width={150}
           height={150}
           className="image-fadeout scale-105 shadow-xl shadow-neutral-950 brightness-100 transition-all duration-200 ease-in group-hover:scale-100 group-hover:brightness-95 group-focus:scale-100 group-focus:brightness-75"
@@ -395,6 +399,7 @@ const FavoritePlaylist = ({
         <button
           type="button"
           tabIndex={0}
+          data-testid="playlist-play-btn"
           className="rounded-full bg-emerald-400 p-2 focus:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500"
           aria-label={
             isPlaying && playlist.id === queueId
@@ -406,6 +411,7 @@ const FavoritePlaylist = ({
             handleCollectionPlayback(
               e,
               playlist,
+              startTransition,
               isPlaying,
               inQueue,
               setQueue,
@@ -424,6 +430,7 @@ const FavoritePlaylist = ({
         <button
           type="button"
           tabIndex={0}
+          data-testid="playlist-remove-btn"
           className="ml-2 rounded-full bg-white p-0 focus:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500"
           aria-label={`Remove playlist ${playlist.name} from favorites`}
           onClick={(e) => {
@@ -449,10 +456,10 @@ const FavoriteSongs = memo(() => {
   return (
     <>
       {songs && songs.length > 0 && (
-        <div>
+        <div data-testid="favorite-songs">
           <h2 className="p-4 py-2 font-semibold text-white">Songs</h2>
           <ul className="flex h-auto max-h-fit w-full flex-col items-start justify-start px-3 py-2">
-            {songs?.map((song: TrackDetails, i) => (
+            {songs.map((song: TrackDetails, i) => (
               <Song index={i} key={song.id} track={song} isWidgetSong={false} />
             ))}
           </ul>
