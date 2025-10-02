4c81f406a4e0a846dc0d983e2e7bdc9e82a86409
tejas-git64 <tejas0586@gmail.com>
2025-10-02 22:21:09 +0530

    added test ids and improved guards

--- src/components/Song/Song.tsx
+++ src/components/Song/Song.tsx
@@ -17,12 +17,12 @@ import notfav from "../../assets/svgs/icons8-heart.svg";
 import fav from "../../assets/svgs/icons8-favorited.svg";
 import playing from "../../assets/gifs/play-animation.gif";
 import { useBoundStore } from "../../store/store";
-import secondsToHMS from "../../utils/utils";
 import add from "../../assets/svgs/icons8-addplaylist-28.svg";
 import tick from "../../assets/svgs/tick.svg";
 import { toggleFavorite } from "../../helpers/toggleFavorite";
 import { cleanString } from "../../helpers/cleanString";
 import { saveToLocalStorage } from "../../helpers/saveToLocalStorage";
+import secondsToHMS from "../../helpers/secondsToHMS";
 
 const Song = memo(
   ({
@@ -34,7 +34,8 @@ const Song = memo(
     isWidgetSong: boolean;
     index: number;
   }) => {
-    const nowPlaying = useBoundStore((state) => state.nowPlaying);
+    const currentTrack = useBoundStore((state) => state.nowPlaying.track);
+    const isPlaying = useBoundStore((state) => state.nowPlaying.isPlaying);
     const songs = useBoundStore((state) => state.favorites.songs);
     const setIsPlaying = useBoundStore((state) => state.setIsPlaying);
     const setNowPlaying = useBoundStore((state) => state.setNowPlaying);
@@ -55,7 +56,7 @@ const Song = memo(
         ids.push(artist.id);
         names.push(artist.name);
       });
-      const playlist: UserPlaylist | undefined = userPlaylists.find((obj) => {
+      const playlist: UserPlaylist | undefined = userPlaylists?.find((obj) => {
         return obj.songs.find((song) => {
           return song.id === track?.id;
         });
@@ -64,7 +65,7 @@ const Song = memo(
     }, [track?.name, userPlaylists]);
 
     const isFavorited = useMemo(
-      () => songs.some((song) => song.id === track?.id),
+      () => songs?.some((song) => song.id === track?.id),
       [songs],
     );
 
@@ -107,6 +108,7 @@ const Song = memo(
           style={{
             transitionDelay: `${index * 10}ms`,
           }}
+          data-testid="song"
           className="song-fadeout group h-12 w-full flex-shrink-0 cursor-pointer overflow-hidden rounded-sm border-b border-neutral-950 text-sm outline-none transition-colors duration-75 ease-in focus-within:bg-neutral-700 hover:bg-neutral-700 focus:bg-neutral-700"
         >
           <div className="flex h-full w-full items-center justify-start p-0 pr-2 transition-all ease-in-out">
@@ -121,14 +123,16 @@ const Song = memo(
             />
             <p
               ref={titleEl}
+              data-testid="name"
               className={`${isWidgetSong ? "w-[10vw] sm:w-[18vw] md:w-[20vw] xmd:w-[22vw] lg:mr-[1vw] lg:w-[22vw] xl:w-[12.5vw] xxl:w-[13.5vw] 2xl:w-[15vw] 2xl:max-w-60" : "w-[40vw] sm:w-[25%] md:w-[30%] lg:w-[25%] xl:w-[30%] 2xl:w-60"} song-fadeout line-clamp-1 flex-shrink-0 flex-grow-[0.85] basis-12 text-ellipsis text-xs font-normal text-white`}
             >
               {(track.name && cleanString(track.name)) || "Unknown track"}
             </p>
             <div
+              data-testid="playing"
               className={`${isWidgetSong ? "mx-[1vw] flex-shrink-0 sm:hidden xmd:mx-2 xmd:block lg:hidden xlg:mx-4 xlg:block xxl:mx-5 2xl:mx-6 2xl:block" : "mx-2 sm:ml-0 lg:mx-8 xl:mx-12 2xl:mx-10"} flex h-5 w-5 flex-shrink-0 items-center justify-start`}
             >
-              {nowPlaying.track?.id === track?.id && nowPlaying.isPlaying && (
+              {currentTrack?.id === track?.id && isPlaying && (
                 <img src={playing} alt="playing" className="h-5 w-5" />
               )}
             </div>
@@ -136,6 +140,7 @@ const Song = memo(
               style={{
                 wordSpacing: "5px",
               }}
+              data-testid="artists"
               className={`${isWidgetSong ? "hidden flex-shrink-0 xlg:flex xlg:w-[3.5vw] xl:w-[5vw] xxl:w-[8.5vw] 2xl:w-[10vw] 2xl:max-w-40" : "hidden sm:mr-12 sm:inline-flex sm:w-[25%] md:mr-6 md:w-[27.5%] xmd:w-[37.5%] lg:mr-10 lg:w-[37.5%] xl:mr-[7%] xl:w-[25%] xxl:mr-[4%] xxl:w-[30%] 2xl:mr-14 2xl:w-[35%] 2xl:max-w-96"} mr-4 line-clamp-1 flex flex-shrink-0 space-x-3 overflow-hidden font-medium text-neutral-300`}
             >
               {artistIds?.map((id, i) => (
@@ -144,6 +149,7 @@ const Song = memo(
             </div>
             <p
               ref={durationEl}
+              data-testid="duration"
               className={`${isWidgetSong ? "mr-[1vw] w-10 flex-shrink-0 sm:ml-[4vw] sm:mr-2 md:mx-[2vw] xmd:mx-[3vw] lg:mx-[1vw] xlg:ml-[1.5vw] xxl:mx-[0.5vw] 2xl:mx-2" : "m-[3vw] w-10 max-w-14 sm:ml-4 sm:mr-[2%] sm:block md:mx-[5%] xmd:mx-4 lg:mx-0 xlg:mx-[2vw] xl:mr-4"} song-fadeout text-xs font-normal text-white duration-200 ease-in`}
             >
               {secondsToHMS(Number(track?.duration))}
@@ -152,6 +158,7 @@ const Song = memo(
               <button
                 tabIndex={0}
                 type="button"
+                data-testid="favorite-btn"
                 className={`h-auto w-[20px] flex-shrink-0 bg-transparent p-0 opacity-0 transition-opacity group-hover:opacity-100 group-focus-visible:opacity-100 group-focus-visible:ring-2 group-focus-visible:ring-emerald-500 ${isFavorited ? "opacity-100" : "opacity-0"}`}
                 onClick={(e) =>
                   track &&
@@ -169,12 +176,13 @@ const Song = memo(
                   width={18}
                   height={18}
                   src={isFavorited ? fav : notfav}
-                  alt="not-fav"
+                  alt="fav-icon"
                 />
               </button>
               <button
                 tabIndex={0}
                 type="button"
+                data-testid="playlist-btn"
                 onClick={(e) => {
                   e.stopPropagation();
                   setCreationTrack(track);
@@ -186,7 +194,7 @@ const Song = memo(
                   width={18}
                   height={18}
                   src={playlist?.id ? tick : add}
-                  alt="tick"
+                  alt="playlist-icon"
                 />
               </button>
             </div>
@@ -213,13 +221,14 @@ const Artist = memo(
       <p
         key={id}
         ref={artistEl}
+        data-testid="artist"
         onClick={(e) => {
           e.stopPropagation();
           id && navigate(`/artists/${id}`);
         }}
         className="song-fadeout cursor-pointer whitespace-nowrap py-4 text-xs text-neutral-400 duration-200 ease-in hover:text-white"
       >
-        {cleanString(artistName) || ""}
+        {artistName ? cleanString(artistName) : "Unknown Artist"}
       </p>
     );
   },
