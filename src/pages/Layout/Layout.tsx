ef634450eda4839d755ab62a45e5ce729a545284
tejas-git64 <tejas0586@gmail.com>
2025-10-02 22:24:21 +0530

    added test ids

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -13,15 +13,20 @@ export default function Layout() {
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   return (
-    <div className="relative h-full w-full overflow-hidden bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-black via-neutral-950 to-neutral-700 2xl:mx-auto 2xl:max-h-[1200px] 2xl:max-w-[1600px]">
+    <div
+      data-testid="layout"
+      className="relative h-full w-full overflow-hidden bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-black via-neutral-950 to-neutral-700 2xl:mx-auto 2xl:max-h-[1200px] 2xl:max-w-[1600px]"
+    >
       {path !== "/" && <Banner />}
       <div
+        data-testid="layout-inner-container"
         className={`relative flex h-full w-full flex-row items-start justify-start overflow-hidden ${
           path !== "/" ? "sm:h-[95vh]" : "sm:h-full"
         }`}
       >
         {path !== "/" && <Nav />}
         <div
+          data-testid="outlet-container"
           className={`h-full w-full overflow-x-hidden ${
             path !== "/" ? "sm:h-[95vh]" : "sm:h-full"
           }`}
@@ -34,6 +39,7 @@ export default function Layout() {
             <Creation ref={containerRef} />
           </div>
           <div
+            data-testid="mobile-features"
             className={`fixed bottom-0 left-0 ${
               path === "/" ? "hidden" : "sm:hidden"
             } flex h-auto w-screen flex-col items-center justify-end bg-gradient-to-t from-black via-[#000000dd] to-transparent pt-2`}
