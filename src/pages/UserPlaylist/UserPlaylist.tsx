8fd00a691b063f0cd9e31d0e93768e9704e7dc1b
tejas-git64 <tejas0586@gmail.com>
2025-10-02 22:26:39 +0530

    added test ids and improved guard checks

--- src/pages/UserPlaylist/UserPlaylist.tsx
+++ src/pages/UserPlaylist/UserPlaylist.tsx
@@ -10,7 +10,7 @@ import {
   UserPlaylist,
 } from "../../types/GlobalTypes";
 import Song from "../../components/Song/Song";
-import { memo, useEffect, useMemo, useRef } from "react";
+import { memo, startTransition, useEffect, useMemo, useRef } from "react";
 import handleCollectionPlayback from "../../helpers/handleCollectionPlayback";
 import { animateScreen } from "../../helpers/animateScreen";
 
@@ -31,17 +31,21 @@ export default function UserPlaylistPage() {
 
   useEffect(() => {
     queue && setNowPlaying(queue.songs[0]);
-  }, [queue?.id]);
+  }, [queue]);
 
   useEffect(() => {
     const localSaves = localStorage.getItem("local-library");
     if (localSaves !== null) {
-      const { userPlaylists }: LocalLibrary = JSON.parse(localSaves);
-      const playlist =
-        userPlaylists?.find(
-          (playlist: UserPlaylist) => playlist.id === Number(id),
-        ) ?? null;
-      if (playlist !== null) setUserPlaylist(playlist);
+      try {
+        const { userPlaylists }: LocalLibrary = JSON.parse(localSaves);
+        const playlist =
+          userPlaylists?.find(
+            (playlist: UserPlaylist) => playlist.id === Number(id),
+          ) ?? null;
+        if (playlist !== null) setUserPlaylist(playlist);
+      } catch (err) {
+        console.error("Failed to parse local library from localStorage:", err);
+      }
     }
     animateScreen(upEl);
   }, []);
@@ -49,6 +53,7 @@ export default function UserPlaylistPage() {
   return (
     <>
       <div
+        data-testid="userplaylist-page"
         ref={upEl}
         className="h-full w-full overflow-x-hidden overflow-y-scroll scroll-smooth"
       >
@@ -60,9 +65,12 @@ export default function UserPlaylistPage() {
           {playlist && <UserPlaylistControls {...playlist} />}
         </div>
         <div className="mx-auto h-auto min-h-[80dvh] w-full bg-neutral-900">
-          {playlist?.songs?.length ? (
-            <ul className="flex h-auto max-h-fit w-full flex-col items-start justify-start bg-transparent px-4 pb-28 pt-4 sm:pb-20">
-              {playlist?.songs.map((song: TrackDetails, i) => (
+          {playlist?.songs ? (
+            <ul
+              data-testid="playlist-container"
+              className="flex h-auto max-h-fit w-full flex-col items-start justify-start bg-transparent px-4 pb-28 pt-4 sm:pb-20"
+            >
+              {playlist.songs.map((song: TrackDetails, i) => (
                 <Song
                   key={song.id}
                   index={i}
@@ -95,8 +103,11 @@ const UserPlaylistInfo = memo(({ name }: { name: string }) => {
         fetchPriority="high"
         className="mr-4 mt-1 h-[150px] w-[150px]"
       />
-      <p className="mt-1 line-clamp-1 h-auto w-[80%] text-ellipsis text-left text-xl font-semibold text-white sm:line-clamp-3 sm:w-[40%] sm:text-3xl sm:font-bold">
-        {name || "Unknown"}
+      <p
+        data-testid="playlist-name"
+        className="mt-1 line-clamp-1 h-auto w-[80%] text-ellipsis text-left text-xl font-semibold text-white sm:line-clamp-3 sm:w-[40%] sm:text-3xl sm:font-bold"
+      >
+        {name}
       </p>
     </div>
   );
@@ -114,7 +125,7 @@ const UserPlaylistControls = memo(({ id, songs, name }: UserPlaylist) => {
   const isPlayable = songs && songs.length > 0;
   const inQueue = useMemo(() => queueId === String(id), [id]);
 
-  const handleKeyDown = () => (e: React.KeyboardEvent) => {
+  const handleKeyDown = (e: React.KeyboardEvent) => {
     if (e.key === "Enter" || e.key === " ") {
       e.preventDefault();
       handleCollectionPlayback(
@@ -125,6 +136,7 @@ const UserPlaylistControls = memo(({ id, songs, name }: UserPlaylist) => {
           name: name,
           songs: songs,
         },
+        startTransition,
         isPlaying,
         inQueue,
         setQueue,
@@ -135,7 +147,10 @@ const UserPlaylistControls = memo(({ id, songs, name }: UserPlaylist) => {
   };
 
   return (
-    <div className="flex w-[100px] items-center justify-between">
+    <div
+      data-testid="userplaylist-controls"
+      className="flex w-[100px] items-center justify-between"
+    >
       <button
         type="button"
         title="Shuffle playlist"
@@ -165,6 +180,7 @@ const UserPlaylistControls = memo(({ id, songs, name }: UserPlaylist) => {
             <path
               d="M16.4697 9.46967C16.1768 9.76256 16.1768 10.2374 16.4697 10.5303C16.7626 10.8232 17.2374 10.8232 17.5303 10.5303L16.4697 9.46967ZM19.5303 8.53033C19.8232 8.23744 19.8232 7.76256 19.5303 7.46967C19.2374 7.17678 18.7626 7.17678 18.4697 7.46967L19.5303 8.53033ZM18.4697 8.53033C18.7626 8.82322 19.2374 8.82322 19.5303 8.53033C19.8232 8.23744 19.8232 7.76256 19.5303 7.46967L18.4697 8.53033ZM17.5303 5.46967C17.2374 5.17678 16.7626 5.17678 16.4697 5.46967C16.1768 5.76256 16.1768 6.23744 16.4697 6.53033L17.5303 5.46967ZM19 8.75C19.4142 8.75 19.75 8.41421 19.75 8C19.75 7.58579 19.4142 7.25 19 7.25V8.75ZM16.7 8L16.6993 8.75H16.7V8ZM12.518 10.252L13.1446 10.6642L13.1446 10.6642L12.518 10.252ZM10.7414 11.5878C10.5138 11.9338 10.6097 12.3989 10.9558 12.6266C11.3018 12.8542 11.7669 12.7583 11.9946 12.4122L10.7414 11.5878ZM11.9946 12.4122C12.2222 12.0662 12.1263 11.6011 11.7802 11.3734C11.4342 11.1458 10.9691 11.2417 10.7414 11.5878L11.9946 12.4122ZM10.218 13.748L9.59144 13.3358L9.59143 13.3358L10.218 13.748ZM6.041 16V16.75H6.04102L6.041 16ZM5 15.25C4.58579 15.25 4.25 15.5858 4.25 16C4.25 16.4142 4.58579 16.75 5 16.75V15.25ZM11.9946 11.5878C11.7669 11.2417 11.3018 11.1458 10.9558 11.3734C10.6097 11.6011 10.5138 12.0662 10.7414 12.4122L11.9946 11.5878ZM12.518 13.748L13.1446 13.3358L13.1446 13.3358L12.518 13.748ZM16.7 16V15.25H16.6993L16.7 16ZM19 16.75C19.4142 16.75 19.75 16.4142 19.75 16C19.75 15.5858 19.4142 15.25 19 15.25V16.75ZM10.7414 12.4122C10.9691 12.7583 11.4342 12.8542 11.7802 12.6266C12.1263 12.3989 12.2222 11.9338 11.9946 11.5878L10.7414 12.4122ZM10.218 10.252L9.59143 10.6642L9.59144 10.6642L10.218 10.252ZM6.041 8L6.04102 7.25H6.041V8ZM5 7.25C4.58579 7.25 4.25 7.58579 4.25 8C4.25 8.41421 4.58579 8.75 5 8.75V7.25ZM17.5303 13.4697C17.2374 13.7626 16.7626 13.7626 16.4697 13.4697C16.1768 13.1768 16.1768 12.702 16.4697 12.4091L17.5303 13.4697ZM18.4697 16.5303C18.7626 16.8232 19.2374 16.8232 19.5303 16.5303C19.8232 16.2374 19.8232 15.7626 19.5303 15.4697L18.4697 16.5303ZM19.5303 16.5303C19.8232 16.2374 19.8232 15.7626 19.5303 15.4697C19.2374 15.1768 18.7626 15.1768 18.4697 15.4697L19.5303 16.5303ZM16.4697 17.4697C16.1768 17.7626 16.1768 18.2374 16.4697 18.5303C16.7626 18.8232 17.2374 18.8232 17.5303 18.5303L16.4697 17.4697ZM17.5303 10.5303L19.5303 8.53033L18.4697 7.46967L16.4697 9.46967L17.5303 10.5303ZM19.5303 7.46967L17.5303 5.46967L16.4697 6.53033L18.4697 8.53033L19.5303 7.46967ZM19 7.25H16.7V8.75H19V7.25ZM16.7007 7.25C14.7638 7.24812 12.956 8.22159 11.8914 9.8398L13.1446 10.6642C13.9314 9.46813 15.2676 8.74861 16.6993 8.75L16.7007 7.25ZM11.8914 9.83979L10.7414 11.5878L11.9946 12.4122L13.1446 10.6642L11.8914 9.83979ZM10.7414 11.5878L9.59144 13.3358L10.8446 14.1602L11.9946 12.4122L10.7414 11.5878ZM9.59143 13.3358C8.80541 14.5306 7.47115 15.25 6.04098 15.25L6.04102 16.75C7.97596 16.7499 9.78113 15.7767 10.8446 14.1602L9.59143 13.3358ZM6.041 15.25H5V16.75H6.041V15.25ZM10.7414 12.4122L11.8914 14.1602L13.1446 13.3358L11.9946 11.5878L10.7414 12.4122ZM11.8914 14.1602C12.956 15.7784 14.7638 16.7519 16.7007 16.75L16.6993 15.25C15.2676 15.2514 13.9314 14.5319 13.1446 13.3358L11.8914 14.1602ZM16.7 16.75H19V15.25H16.7V16.75ZM11.9946 11.5878L10.8446 9.83979L9.59144 10.6642L10.7414 12.4122L11.9946 11.5878ZM10.8446 9.8398C9.78113 8.2233 7.97596 7.25005 6.04102 7.25L6.04098 8.75C7.47115 8.75004 8.80541 9.46939 9.59143 10.6642L10.8446 9.8398ZM6.041 7.25H5V8.75H6.041V7.25ZM16.4697 14.5303L18.4697 16.5303L19.5303 15.4697L17.5303 13.4697L16.4697 14.5303ZM18.4697 15.4697L16.4697 17.4697L17.5303 18.5303L19.5303 16.5303L18.4697 15.4697Z"
               fill="currentColor"
+              data-testid="shuffle-icon"
               className={`${isShuffling ? "fill-emerald-500" : "fill-white"} ease duration-250 transition-colors`}
             />
           </g>
@@ -172,6 +188,7 @@ const UserPlaylistControls = memo(({ id, songs, name }: UserPlaylist) => {
       </button>
       <button
         type="button"
+        data-testid="play-btn"
         title={
           isPlayable
             ? id === Number(queueId) && isPlaying
@@ -199,6 +216,7 @@ const UserPlaylistControls = memo(({ id, songs, name }: UserPlaylist) => {
                     name: name,
                     songs: songs,
                   },
+                  startTransition,
                   isPlaying,
                   inQueue,
                   setQueue,
