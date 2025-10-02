ed3131644aa769436ae434a7c406e47cb9084dc8
tejas-git64 <tejas0586@gmail.com>
2025-10-02 22:23:31 +0530

    added test ids and improved guard checks

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       changeGreeting("Good afternoon");
     } else if (hour > 15 && hour <= 18) {
       changeGreeting("Good evening");
-    } else if (hour > 18 && hour < 6) {
+    } else if (hour > 18) {
       changeGreeting("Good night");
     } else {
       changeGreeting("Jump back in");
@@ -38,8 +38,8 @@ export default function Home() {
   };
 
   function fadeOutNavigate(str: string) {
-    homeRef.current?.classList.add("home-fadeout");
     homeRef.current?.classList.remove("home-fadein");
+    homeRef.current?.classList.add("home-fadeout");
     setTimeout(() => {
       navigate(str);
     }, 150);
@@ -55,6 +55,7 @@ export default function Home() {
 
   return (
     <div
+      data-testid="home-page"
       ref={homeRef}
       className="home-fadeout h-auto max-h-max w-full scroll-smooth duration-300 ease-in-out xl:w-full"
     >
@@ -65,7 +66,7 @@ export default function Home() {
       <TimelyPlaylists fadeOutNavigate={fadeOutNavigate} />
       <div className="h-auto max-h-max w-full pb-28 sm:pb-14">
         {/* Genres */}
-        {genres.map((genre) => (
+        {genres.map((genre: string) => (
           <Section
             genre={genre}
             key={genre}
@@ -77,7 +78,7 @@ export default function Home() {
   );
 }
 
-const Widget = memo(
+export const Widget = memo(
   ({ fadeOutNavigate }: { fadeOutNavigate: (str: string) => void }) => {
     const isPlaying = useBoundStore((state) => state.nowPlaying.isPlaying);
     const setIsPlaying = useBoundStore((state) => state.setIsPlaying);
@@ -94,8 +95,9 @@ const Widget = memo(
       staleTime: 1000 * 60 * 10,
       gcTime: 1000 * 60 * 10,
     });
+
     const lcpImg =
-      data?.image.find((img: Image) => img.quality === "500x500")?.url ??
+      data?.image?.find((img: Image) => img.quality === "500x500")?.url ??
       widgetfallback;
 
     const handlePlaylist = (
@@ -127,6 +129,7 @@ const Widget = memo(
       <Suspense fallback={<Widgetfallback />}>
         <div
           ref={widgetRef}
+          data-testid="widget"
           className="song-fadeout h-auto max-h-max w-full px-3.5 sm:pt-0.5"
         >
           <section className="relative z-0 mx-auto mb-7 flex h-80 w-full flex-col overflow-hidden rounded-sm bg-transparent sm:my-3 sm:h-[40vw] sm:flex-row md:h-80">
@@ -136,6 +139,7 @@ const Widget = memo(
               alt="img"
               fetchPriority="high"
               loading="eager"
+              data-testid="widget-image"
               className="widget-banner-fadeout aspect-square h-auto w-auto flex-shrink-0 cursor-pointer rounded-sm brightness-75 duration-200 ease-in contain-layout sm:z-10 sm:h-[40vw] sm:w-[40vw] sm:brightness-100 md:h-80 md:w-80"
               onClick={() =>
                 data !== null &&
@@ -155,6 +159,7 @@ const Widget = memo(
                 }}
                 tabIndex={data ? 0 : -1}
                 onClick={handlePlaylist}
+                data-testid="widget-btn"
                 className="rounded-full bg-emerald-500 p-1.5 focus-visible:outline-none focus-visible:ring-8 focus-visible:ring-black"
               >
                 <img
@@ -169,7 +174,7 @@ const Widget = memo(
               id="widget-container"
               className="absolute bottom-1.5 left-[1.75%] h-[158px] w-[96.5%] list-none overflow-x-hidden overflow-y-scroll rounded-sm bg-neutral-900 sm:static sm:ml-3 sm:mt-0 sm:h-full"
             >
-              {data && data.songs.length > 0 ? (
+              {data && data.songs?.length > 0 ? (
                 data.songs.map((song: TrackDetails, i: number) => (
                   <Song
                     index={i}
@@ -191,7 +196,7 @@ const Widget = memo(
   },
 );
 
-const TimelyPlaylists = memo(
+export const TimelyPlaylists = memo(
   ({ fadeOutNavigate }: { fadeOutNavigate: (str: string) => void }) => {
     const timeEl = useRef<HTMLDivElement>(null);
     const loadedCount = useRef(0);
@@ -219,10 +224,12 @@ const TimelyPlaylists = memo(
       <Suspense fallback={<TimelyFallback />}>
         <div
           ref={timeEl}
+          data-testid="timely-playlists"
           className="song-fadeout mx-auto mb-4 mt-1 grid h-auto w-full cursor-pointer grid-cols-2 grid-rows-2 gap-3 px-3.5 duration-200 ease-in sm:gap-5"
         >
           <div
             tabIndex={0}
+            data-testid="today-playlist"
             onClick={() => fadeOutNavigate(`/playlists/${viral?.id}`)}
             className="flex h-[50px] w-full items-center justify-start overflow-hidden rounded-sm bg-neutral-800 shadow-sm outline-none transition-all ease-linear hover:text-yellow-500 hover:shadow-md hover:shadow-yellow-500 focus-visible:bg-neutral-700 sm:h-full"
           >
@@ -232,6 +239,7 @@ const TimelyPlaylists = memo(
               width={50}
               height={50}
               fetchPriority="high"
+              data-testid="today-playlist-image"
               loading="eager"
               className="h-[50px] w-[50px] rounded-sm"
               onLoad={handleLoadedImage}
@@ -243,6 +251,7 @@ const TimelyPlaylists = memo(
           </div>
           <div
             tabIndex={0}
+            data-testid="weekly-playlist"
             onClick={() => fadeOutNavigate(`/playlists/${weekly?.id}`)}
             className="flex h-[50px] w-full items-center justify-start overflow-hidden rounded-sm bg-neutral-800 shadow-sm outline-none transition-all ease-linear hover:text-teal-500 hover:shadow-md hover:shadow-teal-500 focus-visible:bg-neutral-700 sm:h-auto"
           >
@@ -253,6 +262,7 @@ const TimelyPlaylists = memo(
               height={50}
               fetchPriority="high"
               loading="eager"
+              data-testid="weekly-playlist-image"
               className="h-[50px] w-[50px] rounded-sm"
               onLoad={handleLoadedImage}
               onError={(e) => (e.currentTarget.src = fallbackweekly)}
@@ -263,6 +273,7 @@ const TimelyPlaylists = memo(
           </div>
           <div
             tabIndex={0}
+            data-testid="monthly-playlist"
             onClick={() => fadeOutNavigate(`/playlists/${monthly?.id}`)}
             className="flex h-12 w-full items-center justify-start overflow-hidden rounded-sm bg-neutral-800 shadow-sm outline-none transition-all ease-linear hover:text-rose-500 hover:shadow-md hover:shadow-rose-500 focus-visible:bg-neutral-700 sm:h-auto"
           >
@@ -273,6 +284,7 @@ const TimelyPlaylists = memo(
               height={50}
               fetchPriority="high"
               loading="eager"
+              data-testid="monthly-playlist-image"
               className="h-[50px] w-[50px] rounded-sm"
               onLoad={handleLoadedImage}
               onError={(e) => (e.currentTarget.src = fallbackmonthly)}
@@ -283,6 +295,7 @@ const TimelyPlaylists = memo(
           </div>
           <div
             tabIndex={0}
+            data-testid="yearly-playlist"
             onClick={() => fadeOutNavigate(`/playlists/${latest?.id}`)}
             className="flex h-[50px] w-full items-center justify-start overflow-hidden rounded-sm bg-neutral-800 shadow-sm outline-none transition-all ease-linear hover:text-purple-500 hover:shadow-md hover:shadow-purple-500 focus-visible:bg-neutral-700 sm:h-auto"
           >
@@ -293,6 +306,7 @@ const TimelyPlaylists = memo(
               height={50}
               fetchPriority="high"
               loading="eager"
+              data-testid="yearly-playlist-image"
               className="h-[50px] w-[50px] rounded-sm"
               onLoad={handleLoadedImage}
               onError={(e) => (e.currentTarget.src = fallbackyearly)}
