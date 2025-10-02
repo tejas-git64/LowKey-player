3fbc17ea3d923fb9bde676426a42b15f58607bac
tejas-git64 <tejas0586@gmail.com>
2025-10-02 22:22:50 +0530

    added test ids

--- src/pages/Artist/Loading.tsx
+++ src/pages/Artist/Loading.tsx
@@ -2,7 +2,7 @@ import SongFallback from "../../components/Song/SongFallback";
 
 export const ArtistInfoFallback = () => {
   return (
-    <div className="h-full w-full bg-neutral-900">
+    <div data-testid="artist-fallback" className="h-full w-full bg-neutral-900">
       <div className="flex h-auto w-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-neutral-600 via-neutral-800 to-black p-4 sm:flex-row sm:items-end sm:justify-between sm:bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))]">
         <div className="flex h-full min-w-[80%] flex-col items-center justify-center text-center sm:h-full sm:w-[70%] sm:flex-row sm:justify-start sm:text-left">
           <div className="h-[150px] w-[150px] animate-pulse bg-neutral-400 sm:mr-4"></div>
@@ -32,7 +32,10 @@ export const ArtistInfoFallback = () => {
 
 export const ArtistAlbumFallback = () => {
   return (
-    <div className="h-[240px] w-full px-4 py-3 pb-12">
+    <div
+      data-testid="artist-album-fallback"
+      className="h-[240px] w-full px-4 py-3 pb-12"
+    >
       <h2 className="text-xl font-semibold text-white">Albums</h2>
       <ul className="mx-auto mt-2.5 flex h-full w-full items-center justify-start overflow-hidden">
         {Array.from({ length: 5 }).map((_, i) => (
@@ -51,7 +54,10 @@ export const ArtistAlbumFallback = () => {
 
 export const ArtistSongsFallback = () => {
   return (
-    <div className="h-auto max-h-fit w-full px-4 py-2 pb-4">
+    <div
+      data-testid="artist-songs-fallback"
+      className="h-auto max-h-fit w-full px-4 py-2 pb-4"
+    >
       <h2 className="pb-2 text-xl font-semibold text-white">Songs</h2>
       <ul className="max-h-auto mx-auto mt-1 h-auto w-full overflow-hidden bg-neutral-900">
         {Array.from({ length: 7 }).map((_, i) => (
