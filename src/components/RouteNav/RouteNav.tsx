f8d438356d2eb97892dc5a45e2a7ba1258cc8f47
tejas-git64 <tejas0586@gmail.com>
2025-10-02 22:19:53 +0530

    added test ids

--- src/components/RouteNav/RouteNav.tsx
+++ src/components/RouteNav/RouteNav.tsx
@@ -6,8 +6,12 @@ export default function RouteNav() {
   const navigate = useNavigate();
 
   return (
-    <div className="max-w-auto flex w-[55px] flex-shrink-0 items-center justify-between sm:w-[60px]">
+    <div
+      data-testid="route-nav"
+      className="max-w-auto flex w-[55px] flex-shrink-0 items-center justify-between sm:w-[60px]"
+    >
       <button
+        data-testid="previous-route"
         tabIndex={0}
         onClick={() => navigate(-1)} // go to previous page
         className="group bg-transparent p-0"
@@ -19,6 +23,7 @@ export default function RouteNav() {
         />
       </button>
       <button
+        data-testid="next-route"
         tabIndex={0}
         onClick={() => navigate(1)} //go to next page
         className="group bg-transparent p-0"
