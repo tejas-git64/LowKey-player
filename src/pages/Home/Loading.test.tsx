e2f6979127dced16e1a4348aa46118baebe6ae2f
tejas-git64 <tejas0586@gmail.com>
2025-11-26 01:10:43 +0530

    removed fallback test

--- src/pages/Home/Loading.test.tsx
+++ src/pages/Home/Loading.test.tsx
@@ -1,14 +1,9 @@
 import { cleanup, render, screen } from "@testing-library/react";
 import { afterEach, expect, test } from "vitest";
-import { Loading, TimelyFallback, Widgetfallback } from "./Loading";
+import { TimelyFallback, Widgetfallback } from "./Loading";
 
 afterEach(() => {
   cleanup();
-});
-
-test("loading UI of Home should render", () => {
-  render(<Loading />);
-  expect(screen.getByTestId("home-fallback")).toBeInTheDocument();
 });
 
 test("loading UI of widget should render", () => {
