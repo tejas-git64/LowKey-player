ef9ef6b4a48a545847e9b73c87d26b5cde585a3b
tejas-git64 <tejas0586@gmail.com>
2025-11-26 00:56:15 +0530

    added timer return for cleaner clearance

--- src/helpers/animateScreen.ts
+++ src/helpers/animateScreen.ts
@@ -1,6 +1,7 @@
 export const animateScreen = (ref: React.RefObject<HTMLDivElement | null>) => {
-  setTimeout(() => {
+  const timer = setTimeout(() => {
     ref.current?.classList.remove("home-fadeout");
     ref.current?.classList.add("home-fadein");
   }, 150);
+  return timer;
 };
