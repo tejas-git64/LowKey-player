18e69061271af94d9dc73c08c2044aa562235ed6
tejas-git64 <tejas0586@gmail.com>
2025-11-26 00:56:53 +0530

    streamlined output

--- src/helpers/secondsToHMS.ts
+++ src/helpers/secondsToHMS.ts
@@ -2,7 +2,8 @@ export default function secondsToHMS(seconds: number) {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
   const secs = Math.floor(seconds % 60);
-  return `${hours !== 0 ? (hours < 10 ? "0" + hours + ":" : hours + ":") : ""}${
+  const paddedHrs = hours < 10 ? "0" + hours + ":" : hours + ":";
+  return `${hours === 0 ? "" : paddedHrs}${
     minutes < 10 ? "0" + minutes : minutes
   }:${secs < 10 ? "0" + secs : secs}`;
 }
