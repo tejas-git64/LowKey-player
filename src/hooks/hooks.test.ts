44751dc874f8c4528c888938a08cbce90d783e65
tejas-git64 <tejas0586@gmail.com>
2025-12-06 16:01:22 +0530

    switched from window to globalThis

--- src/hooks/hooks.test.ts
+++ src/hooks/hooks.test.ts
@@ -10,30 +10,30 @@ afterEach(() => {
 
 describe("useResponsiveLayout hook", () => {
   beforeEach(() => {
-    window.innerWidth = 1024;
+    globalThis.innerWidth = 1024;
   });
 
   test("initially sets isMobile correctly", () => {
-    window.innerWidth = 500;
+    globalThis.innerWidth = 500;
     const { result } = renderHook(() => useResponsiveLayout());
     expect(result.current).toBe(true);
 
-    window.innerWidth = 800;
+    globalThis.innerWidth = 800;
     const { result: result2 } = renderHook(() => useResponsiveLayout());
     expect(result2.current).toBe(false);
   });
 
-  test("updates isMobile state on window resize", () => {
+  test("updates isMobile state on globalThis resize", () => {
     const { result } = renderHook(() => useResponsiveLayout());
 
     act(() => {
-      window.innerWidth = 500;
+      globalThis.innerWidth = 500;
       globalThis.dispatchEvent(new Event("resize"));
     });
     expect(result.current).toBe(true);
 
     act(() => {
-      window.innerWidth = 800;
+      globalThis.innerWidth = 800;
       globalThis.dispatchEvent(new Event("resize"));
     });
     expect(result.current).toBe(false);
