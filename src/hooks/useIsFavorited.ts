d245947e73f86efce1ab33d70e2e3069c38b2176
tejas-git64 <tejas0586@gmail.com>
2026-05-19 00:50:32 +0530

    add guard

--- src/hooks/useIsFavorited.ts
+++ src/hooks/useIsFavorited.ts
@@ -5,7 +5,7 @@ export default function useIsFavorited(id: string) {
   const songs = useBoundStore((state) => state.favorites.songs);
   const { isFavorited } = useMemo(() => {
     return {
-      isFavorited: Boolean(songs.some((song) => song.id === id)),
+      isFavorited: Boolean(songs?.some((song) => song.id === id)),
     };
   }, [songs, id]);
   return isFavorited;
