dd1eaf3508f374e2d4703ae0c10da3579650505b
tejas-git64 <tejas0586@gmail.com>
2026-05-28 21:44:39 +0530

    converted to variable imports

--- src/helpers/getTrackImage.ts
+++ src/helpers/getTrackImage.ts
@@ -1,5 +1,5 @@
 import { TrackDetails } from "../types/GlobalTypes";
-import songfallback from "/fallbacks/song-fallback.webp";
+const songfallback = "/fallbacks/song-fallback.webp";
 
 export const getTrackImage = (
   images: TrackDetails["image"] | undefined,
